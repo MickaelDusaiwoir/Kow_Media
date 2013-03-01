b562ee1998f0e63a03b485419e2d6b9306867736
MickaelDusaiwoir <mikanono01@hotmail.com>
2013-03-01 16:54:54 +0100

    save 2.8

--- rempli_jeu/js/script.js
+++ rempli_jeu/js/script.js
@@ -4,7 +4,7 @@
 
 // -- globals
 
-    var data = new Array({'nom' : '', 'prenom' : '', 'jour': '', 'mois' : '', 'annee' : '', 'email' : '', 'adresse' : '', 'code_postal' : '', 'ville' : '' });
+    var userData = {'nom' : '', 'prenom' : '', 'jour': '', 'mois' : '', 'annee' : '', 'email' : '', 'adresse' : '', 'code_postal' : '', 'ville' : '' };
 
 // -- methods
 
@@ -45,8 +45,6 @@
 
         var tmp, champ, valeur;
 
-        //data[$(this).attr('name')] = $(this).val() ;
-
         tmp = new Array();
 
         if ( $(this).attr('name') !== '' )
@@ -55,19 +53,92 @@
 
             if ( $(this).val() !== '' ) 
                 valeur = $(this).val();
-        }        
+        }   
 
-        data[champ] = valeur;
+        userData[champ] = valeur;
 
-        console.log(data);
-
-       // putData(data);
+        console.log(userData);
     }
 
 
     var putData = function ( url ) {
 
+        var key = new Array('nom' , 'prenom', 'jour', 'mois', 'annee', 'email', 'adresse', 'code_postal', 'ville'),
+            data = {'nom' : '', 'prenom' : '', 'jour': '', 'mois' : '', 'annee' : '', 'email' : '', 'adresse' : '', 'code_postal' : '', 'ville' : '' },
+            cookieArray = {};
+
+        if ( document.cookie )
+        {
+            var tmp = document.cookie.split('=');
+            cookieArray = JSON.parse(tmp[1]);         
+        }
+
+        for (var i = 0; i < key.length; i++) 
+        {
+            if ( userData[key[i]] && cookieArray[key[i]] ) 
+            {
+                if ( userData[key[i]] == cookieArray[key[i]] )
+                {
+                    data[key[i]] = cookieArray[key[i]];
+                }
+                else 
+                {
+                    data[key[i]] = userData[key[i]];
+                }
+            }
+            else 
+            {
+                if ( cookieArray[key[i]] )
+                {
+                    data[key[i]] = cookieArray[key[i]];
+                }
+                else if ( userData[key[i]] )
+                {
+                    data[key[i]] = userData[key[i]];
+                }
+            }
+        }
+        
+        console.log(document.cookie);
+
+        createCookie(data);
+        createUrl(data, url, key);
+
+/*                 
+
+// array to string 
+userDataToString = JSON.stringify(userData);
+
+expires.setTime(today.getTime() + (365*24*60*60*1000));
+document.cookie = cookieName+ "=" + JSON.stringify(cookieArray) + ";expires=" + expires.toGMTString();
+
+*/
     }
+
+    var createCookie = function (data)
+    {
+        var cookieName = 'userData',
+            today = new Date(),
+            expires = new Date();
+
+        // array to string 
+        var dataToString = JSON.stringify(data);
+
+        expires.setTime(today.getTime() + (365*24*60*60*1000));
+        document.cookie = cookieName + "=" + JSON.stringify(dataToString) + ";expires=" + expires.toGMTString();
+    }
+
+    var createUrl = function (data, url, key)
+    {
+        for (var i = 0; i < key.length; i++) 
+        {
+            var tmp = '%' + data[key[i]];
+            url = url.replace(tmp, data[key[i]]);
+        }
+
+        console.log(url);
+    }
+
 
 
     // Fonction s'exécutant dès le chargement de la page
@@ -85,7 +156,7 @@
         $('#annee').on('blur',getData);
         $('#email').on('blur',getData);
         $('#adresse').on('blur',getData);
-        $('#cp').on('blur',getData);
+        $('#code_postal').on('blur',getData);
         $('#ville').on('blur',getData);
 
 		// Si une balise "a" être actionné on appelle la fonction getUrl 
